90a53e6075c29e4a235f051acb4daf9727774bd9
Christian Randleff <54103424+DangelTheMangel@users.noreply.github.com>
2023-05-31 16:03:42 +0200

    updatede documentation for the new changes

--- doxygen/html/search/all_c.js
+++ doxygen/html/search/all_c.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['setup_0',['setup',['../master_8ino.html#a4fc01d736fe50cf5b977f755b675f11d',1,'setup():&#160;master.ino'],['../slave_8ino.html#a4fc01d736fe50cf5b977f755b675f11d',1,'setup():&#160;slave.ino']]],
   ['setupbluetoothconnection_1',['setupBlueToothConnection',['../master_8ino.html#aabec44d551f4da54e5fb45808e722b04',1,'setupBlueToothConnection():&#160;master.ino'],['../slave_8ino.html#aabec44d551f4da54e5fb45808e722b04',1,'setupBlueToothConnection():&#160;slave.ino']]],
-  ['slave_2eino_2',['slave.ino',['../slave_8ino.html',1,'']]]
+  ['slave_2eino_2',['slave.ino',['../slave_8ino.html',1,'']]],
+  ['stopmotors_3',['stopmotors',['../slave_8ino.html#a2dcb5b4a72106e2e963c0b5f718f8aa3',1,'slave.ino']]]
 ];
--- doxygen/html/search/functions_6.js
+++ doxygen/html/search/functions_6.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['setup_0',['setup',['../master_8ino.html#a4fc01d736fe50cf5b977f755b675f11d',1,'setup():&#160;master.ino'],['../slave_8ino.html#a4fc01d736fe50cf5b977f755b675f11d',1,'setup():&#160;slave.ino']]],
-  ['setupbluetoothconnection_1',['setupBlueToothConnection',['../master_8ino.html#aabec44d551f4da54e5fb45808e722b04',1,'setupBlueToothConnection():&#160;master.ino'],['../slave_8ino.html#aabec44d551f4da54e5fb45808e722b04',1,'setupBlueToothConnection():&#160;slave.ino']]]
+  ['setupbluetoothconnection_1',['setupBlueToothConnection',['../master_8ino.html#aabec44d551f4da54e5fb45808e722b04',1,'setupBlueToothConnection():&#160;master.ino'],['../slave_8ino.html#aabec44d551f4da54e5fb45808e722b04',1,'setupBlueToothConnection():&#160;slave.ino']]],
+  ['stopmotors_2',['stopmotors',['../slave_8ino.html#a2dcb5b4a72106e2e963c0b5f718f8aa3',1,'slave.ino']]]
 ];
